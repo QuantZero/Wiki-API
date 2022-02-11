74a5117f32cfc58ea6e3222a1e12902ceb72f7e6
QuantZero <yigitkasapoglu95@gmail.com>
2022-02-11 17:45:43 +0300

    POST, GET and DELETE respond functions added.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 
+//Initialize dependencies.
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -14,15 +15,61 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect("mongoDB://localhost27017/wikiDB" , {useNewUrlParser: true});
+
+//Connect to mongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/wikiDB" , {useNewUrlParser: true});
 
 const articleSchema = {
     title: String, 
     content: String
 };
 
-const Article = mongoose.model("Article",  articleSchema);
-//TODO
+const Article = mongoose.model("Article",  articleSchema); 
+
+
+//GET Route for Articles
+
+app.get("/articles", function(req, res){
+    Article.find(function(err, foundArticles){
+        if(!err){
+            res.send(foundArticles);
+        } else {
+        res.send(err)
+        }
+
+    });
+});
+
+//Respond to POST request
+app.post("/articles", function(req, res){
+    console.log(req.body.title)
+    console.log(req.body.content)
+
+
+const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+});
+
+newArticle.save(function(err){
+    if(!err){
+        res.send("Successfully added a new article");
+        } else {
+        res.send(err);
+        }
+    });
+});
+
+//Respond to DELETE request
+app.delete("/articles", function(req, res) {
+    Article.deleteMany(function(err){
+        if(!err){
+            res.send("Successfully deleted all articles");
+        } else {
+            res.send(err)
+        }
+    });
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
